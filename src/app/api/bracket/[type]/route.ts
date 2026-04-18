f220005663591ebde405f786da89495fe746325b
Bayu Palamarta Wirawan <bayupalamartawirawan@gmail.com>
2026-04-18 19:42:09 +0700

    l

--- src/app/api/bracket/[type]/route.ts
+++ src/app/api/bracket/[type]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { Type } from "@prisma/client";
 
+export const dynamic = "force-dynamic";
 export async function GET(
   req: Request,
   { params }: { params: { type: string } }
--- src/app/api/match/updateEnd/route.ts
+++ src/app/api/match/updateEnd/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export const PUT = async(req: NextRequest)=>{
     const { db } = await import("@/lib/db");
     const formData = await req.formData();
