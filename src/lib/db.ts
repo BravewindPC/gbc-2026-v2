fedf57db1da0938f5ce6e6f9d478caa3cd8f10eb
Bayu Palamarta Wirawan <bayupalamartawirawan@gmail.com>
2026-04-18 21:44:19 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
+// 1. Definisi fungsi pembuat instance
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
+// 2. DI SINI letak blok declare global tersebut
 declare global {
+  // eslint-disable-next-line no-var
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
+// 3. Inisialisasi variabel db menggunakan globalThis
 export const db = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+// 4. Jika bukan production, simpan db ke globalThis agar tidak membuat koneksi baru saat reload
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
